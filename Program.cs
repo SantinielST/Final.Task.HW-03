77ec5c7112384ac3a30419edd67b2b3c3f689a9a
Name <email@example.ru>
2025-01-19 12:51:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Final.Task.HW_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    /// <summary>
+    /// Классы пользователей
+    /// </summary>
+
+    abstract class User
+    {
+        public int id;
+        public string userName;
+        public int phoneNumber;
+        public string eMail;
+    }
+
+    class Customer<TMyOrders> : User
+    {
+        public TMyOrders myOrders;
+    }
+
+    class Courier<TOrder> : User
+    {
+        public TOrder orderHomeDelivery;
+    }
+
+    class Driver<TOrderHD, TOrderSD>
+    {
+        public TOrderHD orderPickPointDelivery;
+        public TOrderSD orderShopDelivery;
+    }
+    
+
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+        public int courierId;
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        public int driverId;
+    }
+
+    class ShopDelivery : Delivery
+    {
+        public int driverId;
+    }
+
+    class Order<TDelivery> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
+
+    class MyOrders<TOrder>
+    {
+        public int userId;
+        public TOrder[] orders;
+    }
+}
+
+
